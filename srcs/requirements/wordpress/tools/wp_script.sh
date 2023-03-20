075021503762b2d1f51a7e4d0cd7de134915c3d9
Muser218 <aogidwndeld@naver.com>
2023-03-20 22:09:42 +0900

    redis setting finished

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -18,22 +18,26 @@ define( 'WP_DEBUG', false );
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
+
+define('WP_REDIS_CONFIG', [
+    'host' => 'redis',
+    'port' => 6379,
+    'database' => 0,
+    'maxttl' => 86400 * 7,
+    'timeout' => 1.0,
+    'read_timeout' => 1.0,
+    'split_alloptions' => true,
+    'debug' => false,
+]);
+define('WP_REDIS_DISABLED', false);
 EOF
-#define( 'WP_REDIS_HOST', 'redis' );
-#define( 'WP_REDIS_PORT', 6379 );
-#define( 'WP_REDIS_TIMEOUT', 1 );
-#define( 'WP_REDIS_READ_TIMEOUT', 1 );
-#define( 'WP_REDIS_DATABASE', 0 );
 
 cd /var/www
-#jseo wp-cli
 wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email --path=/var/www/
-#wp plugin install redis-cache --activate --path=/var/www/
-#wp plugin update --all --path=/var/www/
+wp plugin install redis-cache --activate --path=/var/www/
+wp plugin update --all --path=/var/www/
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --path=/var/www/
-#wp redis enable --path=/var/www/
-
-#wp config create --dbname=$DB_ROOT --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb --dbcharset="utf8"
+wp redis enable --path=/var/www/
 fi
 
-/usr/sbin/php-fpm8 -F
+exec /usr/sbin/php-fpm8 -F
