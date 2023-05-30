14e3b73b15d7f37534323ac8b943447ca8978de5
seunghye218 <seunghye@student.42seoul.kr>
2023-05-31 00:11:01 +0900

    feat: redis enable

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -1,7 +1,5 @@
 #!bin/sh
 
-set -e
-
 if [ ! -f "/var/www/wp-config.php" ];
 then
 
@@ -16,21 +14,17 @@ define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
 \$table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
+
+define( 'WP_REDIS_HOST', '${REDIS_HOST}' );
+define( 'WP_REDIS_PORT', ${REDIS_PORT} );
+define( 'WP_REDIS_DATABASE', ${REDIS_DATABASE} );
+define( 'WP_REDIS_TIMEOUT', ${REDIS_TIMEOUT} );
+define( 'WP_REDIS_READ_TIMEOUT', ${REDIS_READ_TIMEOUT} );
+define('WP_REDIS_DISABLED', false);
+
 if ( ! defined( 'ABSPATH' ) ) {
 define( 'ABSPATH', __DIR__ . '/' );}
 require_once ABSPATH . 'wp-settings.php';
-
-define('WP_REDIS_CONFIG', [
-    'host' => '${REDIS_HOST}',
-    'port' => 6379,
-    'database' => ${REDIS_DATABASE},
-    'maxttl' => ${REDIS_MAXTTL},
-    'timeout' => ${REDIS_TIMEOUT},
-    'read_timeout' => ${REDIS_READ_TIMEOUT},
-    'split_alloptions' => true,
-    'debug' => false,
-]);
-define('WP_REDIS_DISABLED', false);
 EOF
 
 cd /var/www
@@ -40,6 +34,7 @@ wp plugin install redis-cache --activate --path=/var/www/
 wp plugin update --all --path=/var/www/
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --path=/var/www/
 wp redis enable --path=/var/www/
+chmod 777 wp-content
 fi
 
 exec /usr/sbin/php-fpm${PHP_VERSION} -F
